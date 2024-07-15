2a76cacc84936c07fd32d2840bbafc18da7d333c
Purva2003 <purvagrover3@gmail.com>
2024-07-15 12:14:44 -0400

    Update Code_Ques2.py

--- Code_Ques2.py
+++ Code_Ques2.py
@@ -13,5 +13,41 @@
 
 
 
-Solution:
+# Solution:
 
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k = [0,1,2]
+        length = len(nums)
+        final = 0 - length - 1
+        print(final)
+        final_arr =[0] * length
+        count_arr = []
+        
+        for i in k:
+            count_arr.append(nums.count(i))
+
+        for i in range(1, len(count_arr)):
+            count_arr[i] = count_arr[i] + count_arr[i - 1]
+
+        for i in range(-1, -len(nums) - 1, -1):
+            final_arr[count_arr[nums[i]] - 1] = nums[i]
+            count_arr[nums[i]] -= 1
+            # print(final_arr)
+
+        for i in range(len(nums)):
+            nums[i] = final_arr[i]
+
+
+
+
+            
+
+        
+                
+
+        
